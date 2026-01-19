cf73b063df3e449bd4fe91162e9017d24366e6a3
MichelPrincy <157823302+MichelPrincy@users.noreply.github.com>
2026-01-19 07:22:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ class TikTokTaskBot:
             os.system(f'{self.adb} am start -a android.intent.action.VIEW -d "{link}" -p com.waxmoon.ma.gp > /dev/null 2>&1')
             await asyncio.sleep(5)
             os.system(f"{self.adb} input tap {coord_clone}")
-            await asyncio.sleep(30) # Attente chargement vidéo
+            await asyncio.sleep(20) # Attente chargement vidéo
 
             # 2. Réouverture (Refresh)
             os.system(f'{self.adb} am start -a android.intent.action.VIEW -d "{link}" -p com.waxmoon.ma.gp > /dev/null 2>&1')
@@ -190,7 +190,7 @@ class TikTokTaskBot:
             
             # --- STRICT : ATTENTE 10S AVANT INTERACTION ---
             print(f"{YELLOW}⏳ Attente stricte 10s...{RESET}", flush=True)
-            await asyncio.sleep(10)
+            await asyncio.sleep(7)
 
             # ACTION
             action_lower = action.lower()
@@ -376,7 +376,7 @@ class TikTokTaskBot:
 ██║  ██║██║  ██╗██║  ██║███████╗███████╗██║ ╚═╝ ██║
 ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝{RESET}
 {DIM}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━{RESET}
-{WHITE}🤖 BOT AUTOMATION V3.2.0 {DIM}|{RESET} {CYAN}BY MICH{RESET}
+{WHITE}🤖 BOT AUTOMATION V3.2.1 {DIM}|{RESET} {CYAN}BY MICH{RESET}
 {DIM}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━{RESET}
  📱 Status ADB    : {adb_status}
  👥 Comptes        : {WHITE}{acc_count}{RESET}
